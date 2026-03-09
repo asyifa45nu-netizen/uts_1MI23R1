385bd85eee50c86a99babf3c28e046925bb2549b
asyifa45nu-netizen <asyifa45nu@gmail.com>
2026-03-09 10:42:30 +0700

    update home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,6 @@
 <x-app-layout>
     <div class="container">
         <h1>Home</h1>
-        <p>Fakultas dan Jurusan
-Teknik. Ilmu Komputer. Teknik Informatika.
-Ilmu Kesehatan. Lainnya. Farmasi. Kesehatan Masyarakat.
-Ilmu Ekonomi. Akuntansi. Ilmu Ekonomi.</p>
+        <p>ini update home.</p>
 </div>
 </x-app-layout>
