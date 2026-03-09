97eb76d3814b05158eeb5870be89fa577dbdb5ae
asyifa45nu-netizen <asyifa45nu@gmail.com>
2026-03-09 10:43:43 +0700

    update about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,6 @@
 <x-app-layout>
     <div class="container">
         <h1>About</h1>
-        <p>Faktor yang Mempengaruhi UKT
-Program Studi: Setiap program studi (D3, D4, S2) memiliki kelompok UKT yang berbeda. 
-Kelompok Ekonomi: UKT ditentukan berdasarkan data ekonomi keluarga mahasiswa. 
-Tahun Akademik: Besaran UKT dapat berubah setiap tahun, jadi penting untuk melihat pengumuman terbaru di situs Polmed. </p>
+        <p>update about page</p>
 </div>
 </x-app-layout>
